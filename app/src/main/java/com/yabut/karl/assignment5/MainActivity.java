9348125dbb6f40736b3f0341b310dfb54c91c2ae
300699795 <karleisenyabut@hotmail.com>
2015-11-18 11:34:44 -0500

    huehue

--- app/src/main/java/com/yabut/karl/assignment5/MainActivity.java
+++ app/src/main/java/com/yabut/karl/assignment5/MainActivity.java
@@ -1,9 +1,13 @@
 package com.yabut.karl.assignment5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+
+        final RadioGroup group = (RadioGroup) findViewById(R.id.RadioGroup);
+
+        final Intent italianIntent = new Intent(this, ItalianActivity.class);
+        final Intent greekIntent = new Intent(this, GreekActivity.class);
+        final Intent chineseIntent = new Intent(this, ChineseActivity.class);
+        final Intent philippineIntent = new Intent(this, PhilippineActivity.class);
+
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId != -1) {
+                    RadioButton rb = (RadioButton) findViewById(checkedId);
+                    if (rb != null) {
+                        RadioButton btn = (RadioButton) group.getChildAt(group.indexOfChild(rb));
+                        Log.d("Debug", (String) btn.getText());
+                        switch ((String) btn.getText()) {
+                            case "Italian":
+                                //stuff
+                                startActivity(italianIntent);
+                                break;
+                            case "Greek":
+                                //so on
+                                startActivity(greekIntent);
+                                break;
+                            case "Chinese":
+                                startActivity(chineseIntent);
+                                break;
+                            case "Philippine":
+                                startActivity(philippineIntent);
+                                break;
+                        }
+                    }
+                }
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
